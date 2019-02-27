bc2a14eb2089ea1f77e18e83b89ee9b4fd974bc1
CrashGtaFan <gtafan51@gmail.com>
2019-02-27 12:37:18 +0000

    Add message model

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomsController < ApplicationController
   def index
+    @messages = Message.order(:id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :messages
+
   has_secure_password
   before_save { self.email = email.downcase }
   
